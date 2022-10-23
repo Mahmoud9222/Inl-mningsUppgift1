b122ccf0e4be1fb55ab8de4402ad3c1a13e6a739
Mahmoud <hamalawim@gmail.com>
2022-10-23 23:52:48 +0200

    Start

--- InlämningsUppgift1/Program.cs
+++ InlämningsUppgift1/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InlämningsUppgift1.Models;
+using System.Collections.Generic;
+using System.IO;
+
+var contacts = new List<Contact>();
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("##### ADDRESS BOOK #####");
+    Console.WriteLine("1.View Adress book");
+    Console.WriteLine("Q.Exit application");
+    var option = Console.ReadLine();
+
+    if (String.IsNullOrEmpty(option))
+    {
+        Console.WriteLine("You must enter an option");
+        Console.ReadKey();
+    }
+    else
+    {
+        switch (option)
+
+        {
+            case "1":
+
+                Console.Clear();
+                Console.WriteLine("##### CONTACTS - ADRESS BOOK #####");
+                foreach (var item in contacts)
+                    Console.WriteLine($" {item.FullName}, {item.Address}");
+
+                break;
+
+            case "2":
+
+                Console.Clear();
+                Console.WriteLine("##### CONTACTS - ADRESS BOOK #####");
+
+                var contact = new Contact();
+                 
+                Console.Write("First name: ");
+                contact.FirstName = Console.ReadLine();
+
+                Console.Write("Last Name: ");
+                contact.LastName = Console.ReadLine();
+
+
+                Console.Write("Street name: ");
+                contact.Street = Console.ReadLine();
+
+
+                Console.Write("City: ");
+                contact.City = Console.ReadLine();
+
+                contacts.Add(contact);
+                Console.WriteLine("Contact added to adress book");
+                break;
+
+            case "Q":
+                break;
+
+        }
+    } while (true);
